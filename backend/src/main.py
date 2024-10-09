698dc261af28264d563a956b9e0b15fd8a278242
Leonard249 <Leonarddokj@gmail.com>
2024-10-09 10:32:31 +0800

    login

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,21 +1,20 @@
 import os
-
 from dotenv import load_dotenv
-from fastapi import FastAPI
-from fastapi import Request
+from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from starlette.responses import HTMLResponse
 from starlette.templating import _TemplateResponse
 from src.expense_service.router import router as groupRouter
+from src.auth.routes import router as auth_router 
 
 load_dotenv()
 relative_path = "src/"
 
 ApiClient = FastAPI()
 ApiClient.include_router(groupRouter)
+ApiClient.include_router(auth_router, prefix="/auth")  # Include the auth router
 
 templates = Jinja2Templates(directory=(relative_path + "templates"))
-
 
 @ApiClient.get('/', response_class=HTMLResponse)
 async def root(request: Request) -> _TemplateResponse:
