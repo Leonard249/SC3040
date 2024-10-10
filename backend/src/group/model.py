718645cc17a49211b5e81fefd0578c86cfdee540
leewW2k <killerrobbot@gmail.com>
2024-10-10 13:11:12 +0800

    add user to group api

--- backend/src/group/model.py
+++ backend/src/group/model.py
@@ -9,13 +9,18 @@ PyObjectId = Annotated[str, BeforeValidator(str)]
 
 
 class User(BaseModel):
+    group_id: str
+    user_id: str
+
+
+class CreateUser(BaseModel):
     user_id: str
 
 
 class GroupModel(BaseModel):
     id: Optional[PyObjectId] = Field(default= None, alias="_id")
     name: str = Field(...)
-    users: List[User] = []
+    users: List[CreateUser] = []
     model_config = ConfigDict(
         populate_by_name=True,
         arbitrary_types_allowed=True,
