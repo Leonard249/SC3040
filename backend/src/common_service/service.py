9e494c072fd22e156ac6d5e9b2b61dce43fa3688
Wei Xian <45598522+leewW2k@users.noreply.github.com>
2024-10-22 01:09:13 +0800

    user-api: get and update user (#29)

--- backend/src/common_service/service.py
+++ backend/src/common_service/service.py
@@ -20,3 +20,34 @@ class CommonService:
 
         # Print and return the list of users
         return matching_users
+
+    async def get_user(self, user_id):
+        user = await self.user_collection.find_one({"_id": ObjectId(user_id)})
+
+        if not user:
+            return None
+
+        return {
+            "username": user.get("username", "dummy"),
+            "email": user.get("email", "dummy"),
+            "phone": user.get("phone", "99999999"),
+            "pic_url": user.get("pic_url", "")
+        }
+
+    async def update_user(self, user_id, user):
+        query = {"_id": ObjectId(user_id)}
+        new_values = {"$set": {
+            "username": user.username,
+            "email": user.email,
+            "pic_url": user.pic_url,
+            "phone": user.phone
+        }}
+
+        try:
+            result = await self.user_collection.update_one(query, new_values)
+
+            if result.modified_count >= 0:
+                return {"message": "User updated successfully"}
+        except Exception as e:
+            print(f"Error updating user: {e}")
+            return {"error": "An error occurred while updating the user"}
