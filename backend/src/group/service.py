3c7ba1fa4142d925dc3791cf67ee1312d675d945
leewW2k <killerrobbot@gmail.com>
2024-10-25 03:28:46 +0800

    minor edit

--- backend/src/group/service.py
+++ backend/src/group/service.py
@@ -44,8 +44,6 @@ class GroupService:
                     }
                 }
             )
-            if pending_user:
-                await self.pending_user_collection.delete_one({"_id": ObjectId(pending_user_id)})
 
             if existing_user:
                 print(f"User with ID {user_id} is already in the group.")
@@ -63,7 +61,7 @@ class GroupService:
                 }
             )
             if pending_user:
-                await self.pending_user_collection.delete_one({"_id": pending_user})
+                await self.pending_user_collection.delete_one({"_id": ObjectId(pending_user_id)})
 
             if result.modified_count > 0:
                 return await self.group_collection.find_one({"_id": ObjectId(group_id)})
